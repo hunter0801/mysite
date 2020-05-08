9e75f63971a1ab8068e6e8a0673839535d4460d9
hunter0801 <1130266522@qq.com>
2020-05-08 14:49:51 +0800

    部署配置变化

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.development") # 指定settings文件development
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
